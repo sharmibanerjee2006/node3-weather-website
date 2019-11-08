f991c3dc5bf1cb5923f3bcbb697acfb49198e5f5
Sharmi Banerjee <sharmi.banerjee@cognizant.com>
2019-11-08 09:57:33 +0000

    application for Heroku

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ const partialsPath = path.join(__dirname,'../templates/partials')
 hbs.registerPartials(partialsPath)
 
 const app = express()
+const port = process.env.PORT || 3000
 
 app.use(express.static(dirPath))
 app.set('view engine', 'hbs');
@@ -88,6 +89,6 @@ app.get('*', (req,res) => {
     })
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('server is up')
 })
