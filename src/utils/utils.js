72a3982b8eba66b1771d401adac335947f74e4a7
Sharmi Banerjee <sharmi.banerjee@cognizant.com>
2019-11-11 07:57:02 +0000

    weather info added

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -31,7 +31,9 @@ const forecast = (lat, long, cb) => {
            cb('unable to find weather', undefined)
        } else {
           // const body = response.body
-           cb(undefined, body.daily.data[0].summary+" It is currently "+body.currently.temperature+" degrees out. There is a "+body.currently.precipProbability+"% chance of rain")
+           cb(undefined, body.daily.data[0].summary+" It is currently "+body.currently.temperature+" degrees out."+
+            " Maximum temperature "+ body.daily.data[0].temperatureHigh +" Minimum temperature "+ body.daily.data[0].temperatureLow +
+            " There is a "+body.currently.precipProbability+"% chance of rain")
        }
      }) 
   }
